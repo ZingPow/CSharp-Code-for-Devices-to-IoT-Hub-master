fc75eb14cb3605a32bdd46e8cdc58859535356d4
Blake McNeill <mcneillb@ZIngPow.ca>
2018-04-17 21:58:59 -0600

    Added Device Twin sample code

--- SimulatedDevice/Program.cs
+++ SimulatedDevice/Program.cs
@@ -1,9 +1,9 @@
-﻿using System;
+﻿using Microsoft.Azure.Devices.Client;
+using Microsoft.Azure.Devices.Shared;
+using Newtonsoft.Json;
+using System;
 using System.Text;
 using System.Threading.Tasks;
-
-using Microsoft.Azure.Devices.Client;
-using Newtonsoft.Json;
 
 namespace SimulatedDevice
 {
@@ -20,19 +20,40 @@ namespace SimulatedDevice
         static async Task Main(string[] args)
         {
             Console.WriteLine("Simulated device\n");
-            deviceClient = DeviceClient.Create(iotHubUri, new DeviceAuthenticationWithRegistrySymmetricKey(deviceName, deviceKey), TransportType.Mqtt);
+            try
+            {
+                deviceClient = DeviceClient.Create(iotHubUri, new DeviceAuthenticationWithRegistrySymmetricKey(deviceName, deviceKey), TransportType.Mqtt);
 
-            var sendMessageTask = new Task(async () => await SendDeviceToCloudMessagesAsync());
-            sendMessageTask.Start();
+                if (deviceClient != null)
+                {
+                    Twin MyTwin = await deviceClient.GetTwinAsync();
 
-            // setup callbacks for direct methods
-            await deviceClient.SetMethodHandlerAsync("DMTurnOnLED", DMTurnOnLED, null);
-            await deviceClient.SetMethodHandlerAsync("DMTurnOffLED", DMTurnOffLED, null);
-            await deviceClient.SetMethodHandlerAsync("DMToggleLED", DMToggleLED, null);
+                    if (MyTwin != null)
+                        SetLED(MyTwin.Properties.Desired.Contains("LEDStatus") ? (int)MyTwin.Properties.Desired["LEDStatus"] : 0);
 
-            await ReceiveC2dAsync();
+                    deviceClient.SetConnectionStatusChangesHandler(ConnectionStatusChangeHandler);
+
+                    deviceClient.SetDesiredPropertyUpdateCallbackAsync(OnDesiredPropertyChanged, null).Wait();
+
+                    var sendMessageTask = new Task(async () => await SendDeviceToCloudMessagesAsync());
+                    sendMessageTask.Start();
+
+                    // setup callbacks for direct methods
+                    await deviceClient.SetMethodHandlerAsync("DMTurnOnLED", DMTurnOnLED, null);
+                    await deviceClient.SetMethodHandlerAsync("DMTurnOffLED", DMTurnOffLED, null);
+                    await deviceClient.SetMethodHandlerAsync("DMToggleLED", DMToggleLED, null);
+
+                    await ReceiveC2dAsync();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Device connection error: {0}", e.Message);
+            }
+
             Console.ReadLine();
         }
+
         static async Task SendDeviceToCloudMessagesAsync()
         {
             double minTemperature = 20;
@@ -70,13 +91,21 @@ namespace SimulatedDevice
 
                 var messageString = JsonConvert.SerializeObject(telemetryDataPoint);
                 var message = new Message(Encoding.UTF8.GetBytes(messageString));
-                
-                await deviceClient.SendEventAsync(message);
-                Console.WriteLine("{0} > Sending message: {1}", DateTime.Now, messageString);
+
+                try
+                {
+                    await deviceClient.SendEventAsync(message);
+                    Console.WriteLine("{0} > Sending message: {1}", DateTime.Now, messageString);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("D2C exception: {0}", e.Message);
+                }
 
                 await Task.Delay(15000);  //send data every 15 seconds so we have enough data for ML
             }
         }
+
         static async Task ReceiveC2dAsync()
         {
             Console.WriteLine("\nReceiving cloud to device messages from service");
@@ -133,18 +162,61 @@ namespace SimulatedDevice
                 ledstatus = 0;
                 result = "{\"LED\":\"Off\"}";
             }
+
             return Task.FromResult(new MethodResponse(Encoding.UTF8.GetBytes(result), 200));
         }
 
         static void TurnOnLED()
         {
             ledstatus = 1;
+
+            UpdateTwin(ledstatus);
         }
 
         static void TurnOffLED()
         {
             ledstatus = 0;
+
+            UpdateTwin(ledstatus);
         }
 
+        static async void UpdateTwin(int led)
+        {
+            TwinCollection reportedProperties = new TwinCollection();
+            reportedProperties["LEDStatus"] = led;
+            await deviceClient.UpdateReportedPropertiesAsync(reportedProperties);
+        }
+
+        static void ConnectionStatusChangeHandler(ConnectionStatus status, ConnectionStatusChangeReason reason)
+        {
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine();
+            Console.WriteLine("Connection Status Changed to {0}", status);
+            Console.WriteLine("Connection Status Changed Reason is {0}", reason);
+            Console.WriteLine();
+            Console.ResetColor();
+        }
+
+        private static async Task OnDesiredPropertyChanged(TwinCollection desiredProperties, object userContext)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("desired property change:");
+            Console.WriteLine(JsonConvert.SerializeObject(desiredProperties));
+            Console.ResetColor();
+
+            if (desiredProperties.Contains("LEDStatus"))
+                SetLED((int)desiredProperties["LEDStatus"]);
+        }
+        static void SetLED(int ledstatus)
+        {
+            if (ledstatus != 1)
+            {
+                TurnOffLED();
+            }
+            else
+            {
+                TurnOnLED();
+            }
+        }
     }
 }
